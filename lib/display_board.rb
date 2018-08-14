cf066d21a95094274e3bb3fa9ccdf92f0eb6d811
Paula McKinley <pmknyc@optimum.net>
2018-08-14 04:05:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,11 +5,11 @@ puts "A Tic Tac Toe board"
 board = Array.new(9, " ")
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2] }"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5] }"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8] }"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 display_board(board)
 
