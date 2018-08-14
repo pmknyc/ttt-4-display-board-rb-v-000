fb6bd7031add084d073b5f29c23d2b4422c5f172
Paula McKinley <pmknyc@optimum.net>
2018-08-14 04:01:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 # Empty board
-board =   ["O","O","O","O","O","O","O","O","O"]
-# Player "moves" on the board
-#board[4] = "X"
-#board[6] = "O"
+
+board = Array.new(9, "   ")
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 display_board(board)
 
@@ -24,4 +22,12 @@ def display_board(x)
   puts " #{x[6]} | #{x[7]} | #{x[8]}"
 end
 display_board(board)
+
+def display_board(x)
+  puts "#{x[0]}|#{x[1]}|#{x[2]}"
+  puts  "-----------"
+  puts "#{x[3]}|#{x[4]}|#{x[5]}"
+  puts  "-----------"
+  puts "#{x[6]}|#{x[7]}|#{x[8]}"
+end
 =end
